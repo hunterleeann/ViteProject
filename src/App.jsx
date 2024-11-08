5750e154c3dcd11bd669ffe3aa6adac139c53903
hunterleeann <hunterleeann41210@gmail.com>
2024-11-07 17:46:23 -0800

    application updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { puppyList } from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
 
   return (
     <>
-      <div></div>
+      <div>
+        {puppies.map((puppy) => {
+          return <p>{puppy.name}</p>;
+        })}
+      </div>
     </>
   );
 }
